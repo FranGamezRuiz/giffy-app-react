7c9802e7e1f2e6a469d47ced90dfcc48c548cfd3
FranGamezRuiz <gamezruizf@gmail.com>
2022-06-17 12:18:48 +0200

    [ADD] Context

---
+++ src/hooks/useGlobalGif.js
@@ -0,0 +1,8 @@
+import { useContext } from 'react'
+import GifsContext from '../context/GifsContext'
+
+export default function useGlobalGif () {
+    const {gifs} = useContext(GifsContext)
+
+    return gifs
+}
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,12 @@
-import { useContext } from "react"
-import { GifsContext } from "../../context/GifsContext";
+import useGlobalGif from "../../hooks/useGlobalGif";
+import Gif from '../../components/Gif'
 
 export default function Detail ({params}) {
-    const {gifs} = useContext(GifsContext)
-    console.log(gifs)
-    return <h1>Gif con id {params.id}</h1>
+    const gifs = useGlobalGif()
+    
+    const gif = gifs.find(singleGif => 
+        singleGif.id === params.id
+    )
+
+    return <Gif {... gif} />
 }
