9ddab24f530276e322b248d05ece60159701ac3a
FranGamezRuiz <gamezruizf@gmail.com>
2022-06-21 13:43:21 +0200

    [ADD] React.memo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,23 @@
 
-import { useState } from 'react'
+import { useCallback } from 'react'
 import { useLocation} from 'wouter'
 import ListOfGifs from '../../components/ListOfGifs'
 import useGifs from '../../hooks/useGifs'
 import TrendingSearches from '../../components/TrendingSearches'
+import SearchForm from '../../components/SearchForm'
 
 
-export default function Home() {
-    const [keyword, setKeyword] = useState('')
+export default function Home() {    
     const [path, pushLocation] = useLocation()
     const {loading, gifs} = useGifs()
 
-    const handleSubmit = evt => {
-        evt.preventDefault()
+    const handleSubmit = useCallback(({keyword}) => {
         pushLocation(`/search/${keyword}`)
-
-    }
-
-    const handleChange = evt => {
-        setKeyword(evt.target.value)
-    }
+    },[pushLocation])
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <button>Buscar</button> 
-                <input placeholder='Search a gif here...' onChange={handleChange} type='text' value={keyword}></input>
-            </form>
+            <SearchForm onSubmit={handleSubmit} />
             <div className="App-main">
                 <div className="App-results">
                     <h3 className="App-title">Última búsqueda</h3>
