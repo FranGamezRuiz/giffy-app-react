fb62e5abd2c8695c9f5aeff5421454d52c8e402c
FranGamezRuiz <gamezruizf@gmail.com>
2022-06-22 12:19:48 +0200

    [ADD] hook seo

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -2,10 +2,13 @@ import Gif from '../../components/Gif'
 import useSingleGif from "../../hooks/useSingleGif";
 import Spinner from "../../components/Spinner"
 import { Redirect } from 'wouter';
+import useSEO from '../../hooks/useSEO';
 
 export default function Detail ({params}) {
     const {gif, isLoading, isError} = useSingleGif({id: params.id})
-    
+    const title = gif.title ?? ''
+
+    useSEO({description:`Detail of ${title}`,title})
     if(isLoading) return <Spinner />
     if(isError) return <Redirect to='/404' />
     if(!gif) return null
